c9880f19686674746f492e668e46d6ea7a0d536f
Ahun Atajanov <ahunatajanov@gmail.com>
2024-07-29 05:26:33 +0300

    main implemented

---
+++ main.py
@@ -0,0 +1,20 @@
+import logging
+from logs import setup_logger
+from src.news_scraper import NewsScraper
+
+def main():
+    setup_logger()
+    logger = logging.getLogger(__name__)
+
+    # Example parameters, these would be provided by Robocloudworkitem in real case scenario
+    search_phrase = "technology"
+    months = 1
+
+    logger.info(f"Starting the news scraper...")
+    scraper = NewsScraper(search_phrase, months)
+    logger.info("Started processing...")
+    scraper.run()
+    logger.info(f"News scraper finished successfully.")
+
+if __name__ == "__main__":
+    main()
